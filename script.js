ecd0b8da04faf72994ffc6b1c6bdae67a04867a0
Alessia Alessandra <alessiaalessandra@alessias-mbp-2.home>
2020-04-07 15:26:12 -0400

    I am saving my work

---
+++ script.js
@@ -0,0 +1,29 @@
+var halfButton = document.getElementById("half-button")
+var fortuneButton = document.getElementById("fortune-button")
+var restyleButton = document.getElementById("restyle-button")
+
+halfButton.addEventListener("click", halfNumber);
+
+function halfNumber() {
+  var number = document.getElementById("half-input").value;
+  numberHalf = number / 2;
+  alert("Half of " + number + " is " + numberHalf);
+}
+
+fortuneButton.addEventListener("click", fortune);
+
+function fortune() {
+  var name = document.getElementById("fortune-input").value;
+  var fortune = document.getElementById("fortune-output").value;
+  var possibleFortunes = ["'Tis a good day for you :)", "'Tis a very bad day for you :(", "'Tis a solid 'okay' day for you :|"];
+  var fortuneOutput = possibleFortunes[Math.floor(Math.random() * possibleFortunes.length)];
+  alert("Dear " + name + ", your fortune for today is: " + fortuneOutput);
+}
+
+restyleButton.addEventListener("click", restyle);
+
+function restyle() {
+  var colours = ["blue", "red", "green", "yellow"];
+  var randomColour = colours[Math.floor(Math.random() * colours.length)];
+  restyle-output.style.color = randomColour;
+}
